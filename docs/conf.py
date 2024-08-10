6b76fedd24482f914a8dda55abc46edcf18e7a78
MLDataAnalytics <128093454+MLDataAnalytics@users.noreply.github.com>
2024-08-10 13:52:08 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("..))
-sys.path.insert(0, os.path.abspath(os.path.join("..","..")))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(os.path.join('..','..')))
 sys.path.insert(0, os.path.abspath(os.path.join('..','src','pnet')))
 #sys.path.insert(0, os.path.abspath(os.path.join('..','..','src')))
 # -- Project information -----------------------------------------------------
