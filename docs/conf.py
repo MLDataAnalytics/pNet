b3b4ff6593e4e8ad1f8583d2bb7618cd5a549c30
MLDataAnalytics <128093454+MLDataAnalytics@users.noreply.github.com>
2024-08-10 13:03:56 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -90,7 +90,8 @@ html_theme_options = {}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
+#html_static_path = ['_static']
 
 # CSS files to include
 html_css_files = ['theme_overrides.css']
